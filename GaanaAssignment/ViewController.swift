ba678d09df4e242a05a61c4aebfb72bb8f485245
Himanshu Saraswat <himanshu.saraswat@yatra.com>
2019-07-27 12:32:12 +0530

    Initial Commit

---
+++ GaanaAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GaanaAssignment
+//
+//  Created by Himanshu Saraswat on 27/07/19.
+//  Copyright © 2019 Himanshu Saraswat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
